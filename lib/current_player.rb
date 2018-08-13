04e362d6b30d128eca25bce8e61d71e19a6c6a1c
Rachel Grier <rgrier104@gmail.com>
2018-08-13 23:21:54 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
-  board.each do |board|
-    if board == "X" || board == "O"
+  board.each do |pos|
+    if pos == "X" || board == "O"
       counter += 1
     end
   end
